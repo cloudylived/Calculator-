9a7eca2828a60beb50fc0069b759bf53cf8279f8
Yusuf Islam <yusuf786islam321@gmail.com>
2026-07-13 18:07:34 +0100

    Added invalid operation handling

--- README.md
+++ README.md
@@ -9,10 +9,11 @@ A simple Python calculator project built to practise Python fundamentals.
 - Takes two numbers from the user
 - Supports addition, subtraction, multiplication, and division
 - Uses float numbers, allowing decimal calculations
+- Supports invalid operation handling
 
 ## Current Version
 
-Version 0.2 - Added multiple operations
+Version 0.3 - Added invalid operation handlings
 
 ## Planned Improvements
 
--- calculator.py
+++ calculator.py
@@ -1,17 +1,29 @@
 number1 = float(input("First number: "))
 number2 = float(input("Second number: "))
+valid_operation = False 
 
 operation = input("Operation: ")
 if operation == "+":
+    valid_operation = True
     answer = number1 + number2 
 
+
 elif operation == "-":
+    valid_operation = True
     answer = number1 - number2
 
 elif operation == "*":
+    valid_operation = True
     answer = number1 * number2
 
 elif operation == "/":
+    valid_operation = True
     answer = number1 / number2
+
+else:
+    print ("Invalid operation")
     
-print(answer)
+if valid_operation:
+    print(answer)
+
+ 
